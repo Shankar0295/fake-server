5913ef61ac5d7ad63d7aca4f5c57bd84358f08e3
Shankar Kumar <shankarkumar@Shankars-MacBook-Air.local>
2021-11-02 19:11:34 +0530

    second commit

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('loremIpsum.json'); 
+const router = jsonServer.router('loremipsum.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3200; 
+const port = process.env.PORT || 3200;
 
 server.use(middlewares);
 server.use(router);
